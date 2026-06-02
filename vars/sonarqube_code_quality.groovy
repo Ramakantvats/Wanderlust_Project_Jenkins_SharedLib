2ba7f571fc12eee9c651efaa88550ed52956a917
Ramakant <rvvats6366@gmail.com>
2026-06-02 22:35:50 +0530

    Update sonarqube_code_quality.groovy Added comments

--- vars/sonarqube_code_quality.groovy
+++ vars/sonarqube_code_quality.groovy
@@ -3,3 +3,10 @@ def call(){
       waitForQualityGate abortPipeline: false
   }
 }
+
+// Quality gates in sonarqube are some predefined rules which the project code must pass during the anaylysis.
+// Test Coverage > 80%
+// No Critical Vulnerabilities
+// No Blocker Bugs
+
+// If the code violates these rules: Then, Jenkins will stop the deployment.
