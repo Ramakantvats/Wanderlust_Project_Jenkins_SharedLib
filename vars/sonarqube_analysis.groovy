7c249728d6e8b3e460971e1fdf1e392d18ae5ffa
Ramakant <rvvats6366@gmail.com>
2026-06-02 22:22:55 +0530

    Update sonarqube_analysis.groovy

--- vars/sonarqube_analysis.groovy
+++ vars/sonarqube_analysis.groovy
@@ -1,9 +1,9 @@
 def call(String SonarQubeAPI, String Projectname, String ProjectKey){
-  withSonarQubeEnv("${SonarQubeAPI}"){ // in SonarQubeAPI means : "Use the SonarQube server configuration stored in jenkins ->settings -> system named "Sonar"  and this contains sonarqube server URL,TOKEN so that sonar-scanner tool can connect to the server to send reports of bugs and vulneurabilites.
+  withSonarQubeEnv("${SonarQubeAPI}"){     
       sh "$SONAR_HOME/bin/sonar-scanner -Dsonar.projectName=${Projectname} -Dsonar.projectKey=${ProjectKey} -X" //Executing shell cmd on master node linux terminal. $SONAR_HOME:/opt/sonar-scanner  this cmd becomes /opt/sonar-scanner/bin/sonar-scanner and This is the actual program that scans your source code. -x= shows all the details on terminal  for example: loading pluggins... , connecting to sonar-qube server...
   }  //Projectname: wanderlust (project name is just to show in sonarqube server dash., ProjectKey: wanderlust (Project Key is Unique identifier used internally by SonarQube)
 }
-
+// in SonarQubeAPI means : "Use the SonarQube server configuration stored in jenkins ->settings -> system named "Sonar"  and this contains sonarqube server URL,TOKEN so that sonar-scanner tool can connect to the server to send reports of bugs and vulneurabilites.
 //conclusion:
 //1. Connect to the configured SonarQube server
               ↓
