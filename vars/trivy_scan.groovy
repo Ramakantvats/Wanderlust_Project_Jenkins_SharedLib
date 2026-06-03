ec4b5c398efeeb1a8be1acc7abce62dfaf803a48
Ramakant <rvvats6366@gmail.com>
2026-06-03 21:55:04 +0530

    Update trivy_scan.groovy Added comment

--- vars/trivy_scan.groovy
+++ vars/trivy_scan.groovy
@@ -1,3 +1,3 @@
 def call(){
-  sh "trivy fs ."
+  sh "trivy fs ." // trivy: scanning tool , fs: filesystem scan , . : current directory overall: trivy will scan current directory files for any bug or vulneurabilities.
 }
