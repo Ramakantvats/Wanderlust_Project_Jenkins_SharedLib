9af9882442de5e3f9bf9ca29a786a5a476c97a1e
Ramakant <rvvats6366@gmail.com>
2026-06-06 18:32:52 +0530

    Update owasp_dependency.groovy added catchError
    we are getting error in jenkins during executing owasp stage the reason is owasp is creating db to store the file and that db is getting currupt

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,4 +1,11 @@
+// def call(){
+//   dependencyCheck additionalArguments: '--scan ./', odcInstallation: 'OWASP' // --Scan:: means  perform scan for current code directory. odcInstallation:OWASP :: means scanning by using the OWASP dependency check tool and tool name is "OWASP". 
+//   dependencyCheckPublisher pattern: '**/dependency-check-report.xml' //"Find the OWASP report and show it in the Jenkins dashboard". Explanation: This line means that if any vulneurability founds in any library or version so a dependency-check-report.xml file will be created which shows the severity of the vulneurability so this line finds that report and shows on jenkins.  
+// }
+
 def call(){
-  dependencyCheck additionalArguments: '--scan ./', odcInstallation: 'OWASP' // --Scan:: means  perform scan for current code directory. odcInstallation:OWASP :: means scanning by using the OWASP dependency check tool and tool name is "OWASP". 
-  dependencyCheckPublisher pattern: '**/dependency-check-report.xml' //"Find the OWASP report and show it in the Jenkins dashboard". Explanation: This line means that if any vulneurability founds in any library or version so a dependency-check-report.xml file will be created which shows the severity of the vulneurability so this line finds that report and shows on jenkins.  
+    catchError(buildResult: 'SUCCESS', stageResult: 'UNSTABLE') {
+        dependencyCheck additionalArguments: '--scan ./', odcInstallation: 'OWASP'
+    }
+    dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
 }
